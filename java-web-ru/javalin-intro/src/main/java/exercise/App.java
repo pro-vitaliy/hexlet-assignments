b568b1757d82f319720f5a734c647606ce9a90f0
@hexlet/cli <support@hexlet.io>
2024-09-29 23:55:47 +0500

    submit java-web-ru/javalin-intro

--- java-web-ru/javalin-intro/src/main/java/exercise/App.java
+++ java-web-ru/javalin-intro/src/main/java/exercise/App.java
@@ -13,7 +13,7 @@ public final class App {
             config.bundledPlugins.enableDevLogging();
         });
 
-        app.get("/", ctx -> ctx.result("Welcome to Hexlet!"));
+        app.get("/welcome", ctx -> ctx.result("Welcome to Hexlet!"));
 
         return app;
         // END
