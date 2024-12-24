150cd417d0557fd8566c12e6c82306bf4d3f080b
@hexlet/cli <support@hexlet.io>
2024-12-24 22:14:31 +0500

    submit java-spring-boot-ru/service-layer

--- java-spring-boot-ru/service-layer/src/test/java/exercise/controller/BooksControllerTest.java
+++ java-spring-boot-ru/service-layer/src/test/java/exercise/controller/BooksControllerTest.java
@@ -179,6 +179,7 @@ class BooksControllerTest {
         assertThat(task.getAuthor().getId()).isEqualTo(dto.get("authorId"));
     }
 
+    @Test
     public void testDestroy() throws Exception {
         bookRepository.save(testBook);
         var request = delete("/books/{id}", testBook.getId());
